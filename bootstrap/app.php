253114ce36139b1a02b490f9d916c3f3333d7d48
Oscar Raichert <raichertoscar@gmail.com>
2025-03-01 00:07:43 -0300

    :sparkle: feat: adding json response with throwable message to api exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,9 +17,13 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->shouldRenderJsonWhen(function (Request $request) {
-            if ($request->is('api/*')) {
-                return true;
+        $exceptions->respond(function (Response $response, Throwable $th, Request $request) {
+            if ($response->getStatusCode() === 500 && $request->is('api/*')) {
+                return response()->json([
+                    'message' => $th->getMessage(),
+                ], 500);
             }
+
+            return $response;
         });
     })->create();
